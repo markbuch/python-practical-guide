1d17d9620713d659a0a4ae326d33eef9f722571c
Mark Buchanan <markbuch@gmail.com>
2021-04-05 21:40:05 -0400

    Added a for loop and while loop.

--- blockchain.py
+++ blockchain.py
@@ -20,13 +20,18 @@ def get_user_input():
         as a float.) """
     return float(input('Your transaction amount please: '))
 
+
+# Get the first transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount )
+while True:
+    tx_amount = get_user_input()
+    add_value(tx_amount,get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value(tx_amount,get_last_blockchain_value())
+    # Output the blockchain list to the console
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
 
-print(blockchain)
+print('Done!')
