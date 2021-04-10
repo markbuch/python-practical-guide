86bf25beb59acfa7354f77227fda4d5517051f3f
Mark Buchanan <markbuch24@outlook.com>
2021-04-10 19:57:51 -0400

    added a data dictionary for a structure.

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 # Initializing our blockchain list
 blockchain = []
+open_transactions = []
+owner = 'Oscar'
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain """
@@ -7,23 +9,30 @@ def get_last_blockchain_value():
         return None
     return blockchain[-1]
 
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(recipient, sender = owner,  amount=1.0):
     """ Append a new value as well as the last blockchain to the blockchain 
     
     Arguments:
-        :transaction_amount: The amount should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
-    
+        :sender: The sender of the coins
+        :recipient: The recipient of the coins
+        :amount: The amount of coinsa sent with the transaction (default = 12.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction,transaction_amount])
+    transaction = {'sender':sender, 
+    'recipient': recipient, 
+    'amount': amount}
     
+    open_transactions.append(transaction)
+    
+
+def mine_block():
+    pass
+
 def get_transaction_value():
     """ Returns the input of a user (a new transaction amount) 
         as a float.) """
-    user_input =  float(input('Your transaction amount please: '))
-    return user_input
+    tx_recipient = input('Enter the recipient of the transaction:')
+    tx_amount =  float(input('Your transaction amount please: '))
+    return (tx_recipient, tx_amount) # tuple
 
 def get_user_choice():
     user_input = input('Your choice: ')
@@ -71,8 +80,10 @@ while waiting_for_input:
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()
-        add_transaction(tx_amount,get_last_blockchain_value())
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data # unpack the tuple data into variables
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice =="h":
