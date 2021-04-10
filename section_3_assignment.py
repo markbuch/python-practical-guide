7ba306c030903be652d12e6825c22e1a343900e5
Mark Buchanan <markbuch24@outlook.com>
2021-04-10 16:56:28 -0400

    Completed section 3 assignment.

--- section_3_assignment.py
+++ section_3_assignment.py
@@ -3,7 +3,7 @@
 # 2) Add an if  check inside the loop to only output names longer than 5 characters.
 # COMPLETED
 # 3) Add another if  check to see whether a name includes a “n”  or “N”  character.
-
+# COMPLETED
 # 4) Use a while  loop to empty the list of names (via pop() )
 
 # initialize list to store names
@@ -15,9 +15,17 @@ def print_name_length():
     
     for index in range(len(names)):
         name = names[index]
-        if len(name)> 5:
+        if len(name)> 5 and ('n' in name or 'N' in name) :
             print(name + ' is ' + str(len(name)) + ' characters long')
 
+def empty_names_list():
+    """ loop through names list and remove all entries using the pop()"""
+    while len(names) > 0:
+        names.pop(0)
+    else:
+        print('names list has been emptied.')
+
+    
 # Use a while loop to collect names from user
 smash_it = True
 
@@ -25,6 +33,7 @@ while smash_it:
     print('1: Enter a name')
     print('2: Output Names')
     print('3: Output length of each Name')
+    print('x: Clear the list')
     print('q: Quit')
 
     user_choice = str(input())
@@ -36,6 +45,9 @@ while smash_it:
         print(names)    
     elif user_choice == '3':
         print_name_length()
+    elif user_choice == 'x':
+        empty_names_list()
+    
     elif user_choice == 'q':
         print('User chose to exit.')
         smash_it = False
