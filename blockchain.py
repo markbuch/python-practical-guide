37f1e27ed8b9f886ca07aa762a771f340ca2396d
Mark Buchanan <markbuch@gmail.com>
2021-04-10 00:05:20 -0400

    Added range in a for loop

--- blockchain.py
+++ blockchain.py
@@ -36,21 +36,34 @@ def print_blockchain_elements():
         print(block)
 
 def verify_chain():
-    block_index = 0
+    #block_index = 0
     is_valid = True
-    
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        if block[0] == blockchain[block_index - 1]:
+        if blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else: 
             is_valid = False
             break
-        block_index += 1
     return is_valid
-while True:
+
+
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     if block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else: 
+    #         is_valid = False
+    #         break
+    #     block_index += 1
+    # return is_valid
+
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -62,16 +75,16 @@ while True:
         add_transaction(tx_amount,get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
-    elif user_choice == 'q':
-        break
     elif user_choice =="h":
         if len(blockchain) >= 1:
             blockchain[0] = [2]
+    elif user_choice == 'q':
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
         print('Invalid blockchain!')
         break
-    
+else: print('User left!')
 
 print('Done!')
